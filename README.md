c03b32047e9044d23d25fafb37ce99a47a496f18
bobby6killers <bobby6killers>
2014-08-17 16:42:27 +0100

    Fibre v1.0.0.8

---
+++ README.md
@@ -0,0 +1,37 @@
+Fibre (FIBRE)
+
+Nist5 Algorithm
+
+60 second block time
+
+PoW + PoS Hybrid
+
+PoW Ends at Block 43200, (30 days mining, actual estimated 20 - 25 days due to PoS staking.)
+
+1 million coins estimated in PoW
+
+12 hours min coin stake age
+
+5% Annual Interest on Staked coins
+
+No IPO
+
+No Foundation
+
+2% Transparent Pre-mine for PR, Bouties, Competitions. Public ledger will be available
+
+110 Block confirmations to maturity (for mined blocks)
+
+7 block confirmations for coin transfers
+
+25 Coins reward per block during PoW
+
+block 2-200 = 0 coins to help avoid instamining immediatley after launch
+
+================================
+
+TCP port:
+
+Main=7223, RPC=	7224
+
+Testnet_Main=17223, Testnet_RPC=17224
---
+++ src/hashblock.h
@@ -0,0 +1,82 @@
+#ifndef HASHBLOCK_H
+#define HASHBLOCK_H
+
+#include "uint256.h"
+#include "sph_blake.h"
+#include "sph_groestl.h"
+#include "sph_jh.h"
+#include "sph_keccak.h"
+#include "sph_skein.h"
+
+#ifndef QT_NO_DEBUG
+#include <string>
+#endif
+
+#ifdef GLOBALDEFINED
+#define GLOBAL
+#else
+#define GLOBAL extern
+#endif
+
+GLOBAL sph_blake512_context     z_blake;
+GLOBAL sph_groestl512_context   z_groestl;
+GLOBAL sph_jh512_context        z_jh;
+GLOBAL sph_keccak512_context    z_keccak;
+GLOBAL sph_skein512_context     z_skein;
+
+#define fillz() do { \
+    sph_blake512_init(&z_blake); \
+    sph_groestl512_init(&z_groestl); \
+    sph_jh512_init(&z_jh); \
+    sph_keccak512_init(&z_keccak); \
+    sph_skein512_init(&z_skein); \
+} while (0) 
+
+
+#define ZBLAKE (memcpy(&ctx_blake, &z_blake, sizeof(z_blake)))
+#define ZGROESTL (memcpy(&ctx_groestl, &z_groestl, sizeof(z_groestl)))
+#define ZJH (memcpy(&ctx_jh, &z_jh, sizeof(z_jh)))
+#define ZKECCAK (memcpy(&ctx_keccak, &z_keccak, sizeof(z_keccak)))
+#define ZSKEIN (memcpy(&ctx_skein, &z_skein, sizeof(z_skein)))
+template<typename T1>
+inline uint256 Hash9(const T1 pbegin, const T1 pend)
+
+{
+    sph_blake512_context     ctx_blake;
+    sph_groestl512_context   ctx_groestl;
+    sph_jh512_context        ctx_jh;
+    sph_keccak512_context    ctx_keccak;
+    sph_skein512_context     ctx_skein;
+    static unsigned char pblank[1];
+
+#ifndef QT_NO_DEBUG
+    //std::string strhash;
+    //strhash = "";
+#endif
+    
+    uint512 hash[6];
+
+    sph_blake512_init(&ctx_blake);
+    sph_blake512 (&ctx_blake, (pbegin == pend ? pblank : static_cast<const void*>(&pbegin[0])), (pend - pbegin) * sizeof(pbegin[0]));
+    sph_blake512_close(&ctx_blake, static_cast<void*>(&hash[0]));
+
+    sph_groestl512_init(&ctx_groestl);
+    sph_groestl512 (&ctx_groestl, static_cast<const void*>(&hash[0]), 64);
+    sph_groestl512_close(&ctx_groestl, static_cast<void*>(&hash[1]));
+    
+    sph_jh512_init(&ctx_jh);
+    sph_jh512 (&ctx_jh, static_cast<const void*>(&hash[1]), 64);
+    sph_jh512_close(&ctx_jh, static_cast<void*>(&hash[2]));
+    
+    sph_keccak512_init(&ctx_keccak);
+    sph_keccak512 (&ctx_keccak, static_cast<const void*>(&hash[2]), 64);
+    sph_keccak512_close(&ctx_keccak, static_cast<void*>(&hash[3]));
+
+    sph_skein512_init(&ctx_skein);
+    sph_skein512 (&ctx_skein, static_cast<const void*>(&hash[3]), 64);
+    sph_skein512_close(&ctx_skein, static_cast<void*>(&hash[4]));
+
+    return hash[4].trim256();
+}
+
+#endif // HASHBLOCK_H
